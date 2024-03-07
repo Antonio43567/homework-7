d230d2a90575f34d9d8f8b21569d3f2b429fd987
Antonio Cipriano <antonio@MacBook-Air-154.local>
2024-03-07 15:51:46 -0500

    Adding public to the project

--- public/index.php
+++ public/index.php
@@ -5,14 +5,26 @@ require_once "../app/models/User.php";
 require_once "../app/models/Post.php";
 require_once "../app/controllers/MainController.php";
 require_once "../app/controllers/UserController.php";
+require_once "../app/controllers/PostController.php";
+
 use app\controllers\MainController;
 use app\controllers\UserController;
 use app\controllers\PostController;
 
-
 $url = $_SERVER["REQUEST_URI"];
 
-//todo add a switch statement router to route based on the url
-//if it is "/posts" return an array of posts via the post controller
-//if it is "/" return the homepage view from the main controller
-//if it is something else return a 404 view from the main controller
+
+switch ($url) {
+    case "/posts":
+        $postController = new PostController();
+        $posts = $postController->showPosts();
+        break;
+    case "/":
+        $mainController = new MainController(); 
+        $mainController->homePage(); 
+        break;
+    default:
+        $mainController = new MainController(); 
+        $mainController->notFound();
+        break;
+}
